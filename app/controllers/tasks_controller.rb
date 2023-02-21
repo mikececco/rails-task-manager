4f04937ccaee06476daa3e6288b7af82ebd53ae7
Mike Cecconello <mikececco2000@gmail.com>
2023-02-21 15:15:15 +0100

    Added post route and icons to links

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,27 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
   def create
+    @task = Task.new
+  end
+
+  def new
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+    # redirect_to new_task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#create', as: :new_task
+  post 'tasks', to: 'tasks#new'
   get '/tasks/:id', to: 'tasks#show', as: :task
-
 end
