c261b25b50f6e3c0b691199f4f4ee0214ac6ae42
Mike Cecconello <mikececco2000@gmail.com>
2023-02-21 13:14:00 +0100

    Added create feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#create', as: :new_task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
 end
